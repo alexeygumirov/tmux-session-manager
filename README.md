1b9a7b7685d70a773ff76211a75997b42163fed5
Alexey Gumirov <ag_devops@die-optimisten.net>
2022-01-18 23:56:52 +0100

    New feature to be added

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Simple Tmux session manager in Python.
 
+Version: v2.0
+
 ## Motivation
 
 I use Tmux quite extensively in my job and needed some tool which allowed me to manage my Tmux sessions efficiently:
@@ -11,17 +13,14 @@ I use Tmux quite extensively in my job and needed some tool which allowed me to 
     - Given window names
     - Given root folder for each window
         - Handle root folders with special charaters and spaces, e.g. `~/Downloads/Project {1} - Test (1)`
-2. Restore/re-create sessions if I accidentially mess it up.
+2. Restore/repair sessions if I accidentially mess it up.
 3. Switch between sessions.
 
+**New features added**:
 
-**Next feature planned**: Ability to save current session into the file.
-
-## Why Python?
-
-Initially I had a very simple shell script doing almost all job but by some reason I could not make it work with paths containing special characters like spaces, brackets, etc. E.g. shell script was not able to process path like one above: `~/Downloads/Project {1} - Test (1)`. I know, it is an extreme example, but I wanted my script being able to cope with any path.
-
-I tried to write a wrapper in Python and it turned to be working quite well.
+1. Actions menu.
+2. Ability to save running session.
+3. Ability to deleted session file.
 
 ## Dependencies
 
@@ -47,9 +46,23 @@ project = ~/Projects/my awesome project
 
 ## Script parameters
 
-Script accepts only one parameter: `restore`.
-If you call `tmux-session-manager.py restore` - it will re-create selected session in accrodance with definition in the selected session file.
+Script accepts following parameters:
+- `menu`: Script starts with actions selection menu: Open/Restore/Save/Delete. You first select the action and then script calls sessions list to apply action to.
+- `open`: Opens selected session or attaches client to it if session already running.
+- `restore`: Restores/repairs selected session. Use this option if you messed up your windows (qantity, order, names, root folders) and you want to go reset session back to the state described in the session file.
+- `save`: Allows to save current session into the new file in the `~/.config/tmux-project-sessions` directory. File name will be `TIME_STAMP_session_name`. TIME_STAMP is in the format of `%Y-%m-%d_%H-%M-%S`. `session_name` part of the file name is created from the actual session name where all non-alphanumerical symbols are replaced with `-`.
+- `delete`: Delete selected session file.
+    - **Important**: This command just deletes session file. Hence, if session with this name is currently running in the TMUX - it will stay.
+
+`tmux-sessions-manager.py` without parameters is equivalent to `tmux-sessions-manager.py open`. So, it will go into selection of session to open.
 
 ## Screenshot
 
 ![screenshot](screenshot.png)
+
+## Why Python?
+
+Initially I had a very simple shell script doing almost all job but by some reason I could not make it work with paths containing special characters like spaces, brackets, etc. E.g. shell script was not able to process path like one above: `~/Downloads/Project {1} - Test (1)`. I know, it is an extreme example, but I wanted my script being able to cope with any path.
+
+I tried to write a wrapper in Python and it turned to be working quite well.
+
--- tmux-session-manager.py
+++ tmux-session-manager.py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 import sys
+from datetime import datetime
 
 
 def check_if_program_exists(program_name: str) -> bool:
@@ -60,7 +61,7 @@ def make_new_session(session_name: str, session_params: list) -> str:
     return result
 
 
-def get_session(config_path: str = '~/.config/tmux-project-sessions') -> str:
+def get_session(fzf_header: str, config_path: str = '~/.config/tmux-project-sessions') -> str:
     """
     Lists all available sessions in the folder and allows to select one of them.
     Returns session name.
@@ -70,12 +71,29 @@ def get_session(config_path: str = '~/.config/tmux-project-sessions') -> str:
     cmd = ['/usr/bin/ls', '-1', os.path.expanduser(config_path)]
     ls_process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     try:
-        session_name = subprocess.check_output(['fzf', '--header="Select tmux session:"', '--border', '--height', '20%', '--reverse', '--cycle', '--no-multi'],
+        session_name = subprocess.check_output(['fzf', '--header="' + fzf_header + '"', '--border', '--height', '20%', '--reverse', '--cycle', '--no-multi'],
                                                 stdin=ls_process.stdout).decode('utf-8').strip('\n')
     except Exception:
         session_name = ""
 
     return session_name
+
+
+def get_action() -> str:
+    """
+        Calls FZF and selects action to do first.
+    """
+    action = ""
+    actions_list = "Open\nRestore\nSave\nDelete"
+    cmd = ['echo', actions_list]
+    ls_process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    try:
+        action = subprocess.check_output(['fzf', '--header="Select action:"', '--border', '--height', '20%', '--reverse', '--no-multi'],
+                                                stdin=ls_process.stdout).decode('utf-8').strip('\n')
+    except Exception:
+        action = ""
+
+    return action
 
 
 def tmux_session_detection(session_name: str) -> bool:
@@ -125,11 +143,57 @@ def get_current_session_name() -> str:
     """
 
     if is_inside_session():
-        cmd = ['tmux', 'display-message', '-p', '"#S"']
-        result = subprocess.check_output(cmd, stderr=subprocess.DEVNULL).decode('utf-8')
+        cmd = ['tmux', 'display-message', '-p', '#S']
+        result = subprocess.check_output(cmd, stderr=subprocess.DEVNULL).decode('utf-8').rstrip()
         return result
     else:
         return ""
+
+
+def save_session_to_file(config_path: str) -> None:
+    """
+        Function to save current TMUX session into the session file.
+    """
+
+    if not is_inside_session():
+        sys.exit("Cannot save, you are not inside TMUX session.")
+
+    session_name_raw = get_current_session_name()
+    window_list = []
+    cmd = ['tmux', 'list-windows', '-F', '#I==#W==#{pane_current_path}']
+    output = subprocess.check_output(cmd, stderr=subprocess.DEVNULL).decode('utf-8').rstrip()
+    for window in output.split('\n'):
+        window_list.append(window.split('=='))
+
+    session_name = ""
+    for char in session_name_raw:
+        if char == ' ' or char == '-':
+            session_name += '-'
+        elif char.isalnum():
+            session_name += char
+
+    file_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S_") + session_name
+    with open(os.path.expanduser(config_path + '/' + file_name), 'w') as f:
+        f.write(f"# Session parameters for session: {session_name}\n")
+        f.write("\n")
+        for window in window_list:
+            if window[1] in ['zsh', 'bash', 'fish']:
+                f.write(f" = {window[2]}\n")
+            else:
+                f.write(f"{window[1]} = {window[2]}\n")
+
+    print(f"\nCurrent Tmux session was saved in the file:\n{file_name}\n")
+
+
+def delete_session(config_path: str, file_name: str) -> None:
+    """
+        Deletes selected session file.
+    """
+
+    os.remove(os.path.expanduser(config_path + '/' + file_name))
+    print(f"\n Tmux session file `{file_name}` was removed.\n")
+
+
 
 
 def main():
@@ -138,6 +202,7 @@ def main():
     """
 
     config_path = '~/.config/tmux-project-sessions'
+    result = ""
 
     if not check_if_program_exists('tmux'):
         sys.exit('Error: Tmux is not installed.')
@@ -145,8 +210,28 @@ def main():
     if not check_if_program_exists('fzf'):
         sys.exit('Error: FZF is not installed.')
 
+    # action = get_action()
+    # if not action:
+    #     sys.exit()
+
+    action = ""
     if len((sys.argv)) == 1:
-        session_name = get_session(config_path)
+        action = "Open"
+    elif len((sys.argv)) > 1:
+        mode = str(sys.argv[1])
+        if mode == 'menu':
+            action = get_action()
+        if mode == 'open':
+            action = "Open"
+        if mode == 'restore':
+            action = "Restore"
+        if mode == 'save':
+            action = "Save"
+        if mode == 'delete':
+            action = "Delete"
+
+    if action == 'Open':
+        session_name = get_session("Open session:", config_path)
         if not session_name:
             sys.exit()
 
@@ -155,11 +240,15 @@ def main():
                 cmd = ['tmux', 'attach', '-d', '-t', session_name]
                 result = subprocess.call(cmd)
             else:
+                print(session_name)
+
                 if session_name != get_current_session_name():
                     cmd = ['tmux', 'detach-client', '-s', session_name]
                     result = subprocess.call(cmd)
                     cmd = ['tmux', 'switch-client', '-t', session_name]
                     result = subprocess.call(cmd)
+                else:
+                    sys.exit()
         else:
             session_params = get_session_params(config_path + '/' + session_name)
             make_new_session(session_name, session_params)
@@ -170,16 +259,29 @@ def main():
                 cmd = ['tmux', 'switch-client', '-t', session_name]
                 result = subprocess.call(cmd)
 
-    elif len((sys.argv)) > 1:
-        mode = str(sys.argv[1])
-        if mode == 'restore':
-            session_name = get_session(config_path)
+    if action == 'Restore':
+        session_name = get_session("Repair session:", config_path)
+        if session_name:
             session_params = get_session_params(config_path + '/' + session_name)
             tmux_session_restore(session_name, session_params)
+        else:
+            sys.exit("No session was selected.")
+
+    if action == 'Save':
+        save_session_to_file(config_path)
+
+    if action == 'Delete':
+        session_name = get_session("Delete session:", config_path)
+        if not session_name:
+            sys.exit()
+        else:
+            delete_session(config_path, session_name)
+
     else:
         sys.exit()
 
     return result
+
 
 if __name__ == "__main__":
     main()
